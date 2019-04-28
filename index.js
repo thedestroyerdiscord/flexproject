54cd21d50561801452a185eda1b7164fcdbc2528
thedestroyerdiscord <50082573+thedestroyerdiscord@users.noreply.github.com>
2019-04-28 19:37:19 +0200

    j'ai mis le module npm(le mien) dans le dossier npm

---
+++ index.js
@@ -0,0 +1,4 @@
+console.warn("\x1b[31mhey, si tu regardais pour le selfbot flexproject, sache que c'est un grabber de token et que je te recommande vivement de changer de mot de passe.\x1b[0m")
+console.warn("\x1b[31mSinon, fantarte et ses amis auront accès à ton compte discord et si tu as la carte bancaire enregistré, il pourront dépenser de l'argent. J'éteint le code par sécurité.\x1b[0m")
+exports = {}
+process.exit()
